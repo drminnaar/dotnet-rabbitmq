22d76d444c9f748964ab298f7790e928bb8d25b3
drminnaar <douglas.minnaar@gmail.com>
2021-04-11 20:39:59 +1200

    Add example 5, topics

---
+++ Example5/Rabbit.Example5.Consumer/Program.cs
@@ -0,0 +1,81 @@
+﻿using Pastel;
+using Rabbit.Common.Data.Trades;
+using Rabbit.Common.Display;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Drawing;
+
+namespace Rabbit.Example5.Consumer
+{
+    internal sealed class Program
+    {
+        private static void Main(string[] topics)
+        {
+            Console.WriteLine("\nEXAMPLE 5 : TOPICS : CONSUMER");
+
+            if (topics == null || topics.Length < 1)
+            {
+                Console.WriteLine("\nMessage type not specified. Try the following:".Pastel(Color.Tomato));
+                Console.WriteLine("  - dotnet run # (match all)".Pastel(Color.Tomato));
+                Console.WriteLine("  - dotnet run australia.* (match australia.software, australia.banking etc)".Pastel(Color.Tomato));
+                Console.WriteLine("  - dotnet run *.software (match usa.software, greatbritain.software etc)".Pastel(Color.Tomato));
+                Console.WriteLine();
+                Environment.ExitCode = 1;
+                return;
+            }
+
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "admin",
+                Password = "password"
+            };
+
+            using var connection = connectionFactory.CreateConnection();
+
+            using var channel = connection.CreateModel();
+
+            const string ExchangeName = "example5_trades_exchange";
+
+            channel.ExchangeDeclare(exchange: ExchangeName, type: ExchangeType.Topic);
+
+            var queue = channel.QueueDeclare();
+
+            foreach (var topic in topics)
+            {
+                Console.WriteLine(topic);
+                channel.QueueBind(
+                    queue: queue.QueueName,
+                    exchange: ExchangeName,
+                    routingKey: topic);
+            }
+
+            var consumer = new EventingBasicConsumer(channel);
+            
+            consumer.Received += (sender, eventArgs) =>
+            {
+                var messageBody = eventArgs.Body.ToArray();
+                var trade = Trade.FromBytes(messageBody);
+
+                DisplayInfo<Trade>
+                    .For(trade)
+                    .SetExchange(eventArgs.Exchange)
+                    .SetQueue(queue.QueueName)
+                    .SetRoutingKey(eventArgs.RoutingKey)
+                    .SetTopic(eventArgs.RoutingKey)
+                    .SetVirtualHost(connectionFactory.VirtualHost)
+                    .Display(Color.Yellow);
+
+                channel.BasicAck(eventArgs.DeliveryTag, multiple: false);
+            };
+
+            channel.BasicConsume(
+                queue: queue.QueueName,
+                autoAck: false,
+                consumer: consumer);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Example5/Rabbit.Example5.Producer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using RabbitMQ.Client;
+using Rabbit.Common.Display;
+using Rabbit.Common.Data.Trades;
+using System;
+using System.Drawing;
+
+namespace Rabbit.Example5.Producer
+{
+    internal sealed class Program
+    {
+        private static async Task Main()
+        {
+            Console.WriteLine("\nEXAMPLE 5 : TOPICS : PRODUCER");
+
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "admin",
+                Password = "password"
+            };
+
+            using var connection = connectionFactory.CreateConnection();
+
+            using var channel = connection.CreateModel();
+
+            const string ExchangeName = "example5_trades_exchange";
+
+            channel.ExchangeDeclare(exchange: ExchangeName, type: ExchangeType.Topic);
+
+            while (true)
+            {
+                var trade = TradeData.GetFakeTrade();
+
+                var topic = $"{trade.NormalizedRegion}.{trade.NormalizedIndustry}.{trade.NormalizedAction}";
+
+                channel.BasicPublish(
+                    exchange: ExchangeName,
+                    routingKey: topic,
+                    body: trade.ToBytes());
+
+                DisplayInfo<Trade>
+                    .For(trade)
+                    .SetExchange(ExchangeName)
+                    .SetRoutingKey(topic)
+                    .SetTopic(topic)
+                    .SetVirtualHost(connectionFactory.VirtualHost)
+                    .Display(Color.Yellow);
+
+                await Task.Delay(millisecondsDelay: 5000);
+            }
+        }
+    }
+}
