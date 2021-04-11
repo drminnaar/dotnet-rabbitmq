5385617e71d7c489f9acea2f2c5528f31ecc7cac
drminnaar <douglas.minnaar@gmail.com>
2021-04-11 20:38:42 +1200

    Add example 3, publish/subscribe

---
+++ Example3/Rabbit.Example3.Consumer/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Drawing;
+using Rabbit.Common.Data.Weather;
+using Rabbit.Common.Display;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Rabbit.Example3.Consumer
+{
+    internal sealed class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("\nEXAMPLE 3 : PUB/SUB : CONSUMER");
+
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "admin",
+                Password = "password"
+            };
+
+            var connection = connectionFactory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            var queueName = channel.QueueDeclare().QueueName;
+
+            const string ExchangeName = "example3_forecasts_exchange";
+
+            channel.ExchangeDeclare(exchange: ExchangeName, type: ExchangeType.Fanout);
+
+            channel.QueueBind(
+                queue: queueName,
+                exchange: ExchangeName,
+                routingKey: string.Empty);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (sender, eventArgs) =>
+            {
+                var body = eventArgs.Body.ToArray();
+                var forecast = Forecast.FromBytes(body);
+
+                DisplayInfo<Forecast>
+                    .For(forecast)
+                    .SetExchange(eventArgs.Exchange)
+                    .SetQueue(queueName)
+                    .SetRoutingKey(eventArgs.RoutingKey)
+                    .SetVirtualHost(connectionFactory.VirtualHost)
+                    .Display(Color.Yellow);
+
+                channel.BasicAck(eventArgs.DeliveryTag, multiple: false);
+            };
+
+            channel.BasicConsume(
+                queue: queueName,
+                autoAck: false,
+                consumer: consumer);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Example3/Rabbit.Example3.Producer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using Rabbit.Common.Data.Weather;
+using Rabbit.Common.Display;
+using RabbitMQ.Client;
+
+namespace Rabbit.Example3.Producer
+{
+    internal sealed class Program
+    {
+        private static async Task Main()
+        {
+            Console.WriteLine($"\nEXAMPLE 3 : PUB/SUB : PRODUCER)");
+
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "admin",
+                Password = "password"
+            };
+
+            using var connection = connectionFactory.CreateConnection();
+
+            using var channel = connection.CreateModel();
+
+            const string QueueName = "";
+
+            const string ExchangeName = "example3_forecasts_exchange";
+
+            channel.ExchangeDeclare(exchange: ExchangeName, type: ExchangeType.Fanout);
+
+            while (true)
+            {
+                var forecast = Thermometer.Fake().Report();
+
+                channel.BasicPublish(
+                    exchange: ExchangeName,
+                    routingKey: QueueName,
+                    body: Encoding.UTF8.GetBytes(forecast.ToJson())
+                );
+
+                DisplayInfo<Forecast>
+                    .For(forecast)
+                    .SetExchange(ExchangeName)
+                    .SetQueue(QueueName)
+                    .SetRoutingKey(QueueName)
+                    .SetVirtualHost(connectionFactory.VirtualHost)
+                    .Display(Color.Yellow);
+
+                await Task.Delay(millisecondsDelay: 3000);
+            }
+        }
+    }
+}
